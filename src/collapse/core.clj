9d1649a9acacfc2736550d1fdd32279aad33239a
Stefan W <sw1@monton.de>
2015-10-17 21:07:06 -0400

    changed recursive function to loop construct

--- src/collapse/core.clj
+++ src/collapse/core.clj
@@ -6,26 +6,22 @@
   [& args]
   (println "Hello, World!"))
 
-(defn reduceDistance
-  ""
-  [pairs remainingDistance]
-  (if (< remainingDistance 0)
-    false
-    (if (empty? pairs)
-      true
-      (let [[p1 p2] (first pairs) pr (rest pairs)]
-        (if (= p1 p2)
-          (reduceDistance pr remainingDistance)
-          (reduceDistance pr (- remainingDistance 1))
+(defn isInEditDistance
+  "Check edit distance of two strings"
+  [str1 str2 maxDist]
+  (loop [pairs (map vector str1 str2) remainingDist maxDist]
+    (if (< remainingDist 0)
+      false
+      (if (empty? pairs)
+        true
+        (let [[p1 p2] (first pairs)]
+          (if (= p1 p2)
+            (recur (rest pairs) remainingDist)
+            (recur (rest pairs) (dec remainingDist))
+            )
           )
         )
       )
     )
-  )
-
-(defn isInEditDistance
-  "Check edit distance of two strings"
-  [str1 str2 maxDist]
-  (reduceDistance (map vector str1 str2) maxDist)
   )
 
